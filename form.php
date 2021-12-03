9c841f1199179b4e394f5db9f8f64a1b1ca8d472
MckyBSc <93104406+MckyBSc@users.noreply.github.com>
2021-12-03 10:35:46 +0800

    form

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Form</title>
+</head>
+<body>
+	
+	<form method="post" action="form.php">
+		First Name:<input type="text" name="f_name" required>
+		<br>
+		Last name<input type="text" name="l_name" required>
+		<br>
+		Email:<input type="email" name="email" required>
+		<br>
+		<input type="submit" name="submit">
+	</form>
+
+</body>
+</html>
